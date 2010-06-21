4f646eb477415acecde24c08aec0c667bf117619
Andrés Mejía <andmej@gmail.com>
2010-06-21 03:34:48 -0500

    Capifying

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+role :app, "69.164.213.254"
+role :web, "69.164.213.254"
+role :db, "69.164.213.254", :primary => true
+
+set :user, "deconjugate"
+set :deploy_to, "/home/#{user}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:andmej/deconjugate.it.git"
+set :branch, "master"
+
+namespace :deploy do
+  desc "Tell Passenger to restart."
+  task :restart, :roles => :web do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+  
+  desc "Do nothing on startup so we don't get a script/spin error."
+  task :start do
+    puts "You may need to restart Apache."
+  end
+
+  desc "Symlink extra configs and folders."
+  task :symlink_extras do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after "deploy:update_code", "deploy:symlink_extras"
