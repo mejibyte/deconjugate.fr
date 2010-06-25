98281d21d30d6e697a40efd54a85bb6d1404dc4a
Andrés Mejía <andmej@gmail.com>
2010-06-24 19:43:18 -0500

    Styling the search result page

--- app/helpers/verbs_helper.rb
+++ app/helpers/verbs_helper.rb
@@ -6,4 +6,8 @@ module VerbsHelper
   def reverse_words(string)
     string.split.reverse.join(" ")
   end
+  
+  def example_link(text, verb = nil)
+    content_tag :span, link_to(text, "#", :rel => verb || text), :class => "examples"
+  end
 end
