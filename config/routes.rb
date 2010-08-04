a3f38e1f25633d12cfa45b389b3a4c212c765f57
Andrés Mejía <andmej@gmail.com>
2010-08-04 01:21:31 -0500

    Adding alternative route to save a few keystrokes when writing the address directly on the address bar: Now you can type "www.deconjugate.fr/voulais" and get directly to the verb "vouloir".

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :verbs, :collection => { :search => :get }
   map.root :controller => "verbs", :action => "search"
+  map.connect "/:s", :controller => "verbs", :action => "index"
 end
